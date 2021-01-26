468a3f091fd9ffc24d45c917a265324409477975
iigonzoii <a.gonzalez418@gmail.com>
2021-01-25 21:38:59 -0700

    comments and some pseudo code in js, adjusted id's in html

---
+++ main.js
@@ -0,0 +1,28 @@
+
+
+// variables to store the individual time blocks for reference
+
+
+// this will go in a function(startDay) because the day changes every 24 hours and I want this to be the first thing called
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
+
+// function to "start or set planner day"
+function startDay (){
+
+}
+
+startDay()
+
+
+
+// event listener that triggers save buttons to save stuff into local storage
+
+
+
+// function that tells us what timeblock we are in using color, what timeblocks we have already been in by muting color, and what time blocks we have left by using a different color
+
+function futurePastPresent(){
+    
+}
+
+
