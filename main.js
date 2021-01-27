763601b4c17c2d238458a0ac3b11540f672511e6
iigonzoii <a.gonzalez418@gmail.com>
2021-01-26 17:05:08 -0700

    created function for new day

--- main.js
+++ main.js
@@ -4,11 +4,20 @@
 
 
 // this will go in a function(startDay) because the day changes every 24 hours and I want this to be the first thing called
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
+
 
 // function to "start or set planner day"
 function startDay (){
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
 
+    $(".time-block").each(function(){
+        var id = $(this).attr("id");
+        var schedule = localStorage.getItem(id);
+
+        if (schedule !== null) {
+            $(this).children(".schedule").val(schedule);
+        }
+    });
 }
 
 startDay()
