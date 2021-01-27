f8f440ecd2b32ee92f04517d2f67f3c414662972
iigonzoii <a.gonzalez418@gmail.com>
2021-01-26 17:11:58 -0700

    added variables for the id's i set

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour1 = $("#13");
+var hour2 = $("#14");
+var hour3 = $("#15");
+var hour4 = $("#16");
+var hour5 = $("#17");
+var time = moment();
 
 
 // variables to store the individual time blocks for reference
