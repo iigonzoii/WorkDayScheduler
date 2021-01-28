bb028d253e71a83cb4cd79ecf253eeaf66c4486f
iigonzoii <a.gonzalez418@gmail.com>
2021-01-28 09:22:00 -0700

    starting js from scratch

--- main.js
+++ main.js
@@ -1,47 +1,38 @@
-var hour9 = $("#9");
-var hour10 = $("#10");
-var hour11 = $("#11");
-var hour12 = $("#12");
-var hour1 = $("#13");
-var hour2 = $("#14");
-var hour3 = $("#15");
-var hour4 = $("#16");
-var hour5 = $("#17");
-var time = moment();
-
-
-// variables to store the individual time blocks for reference
-
-
-// this will go in a function(startDay) because the day changes every 24 hours and I want this to be the first thing called
-
-
-// function to "start or set planner day"
-function startDay (){
-    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"))
-
-    $(".time-block").each(function(){
-        var id = $(this).attr("id");
-        var schedule = localStorage.getItem(id);
-
-        if (schedule !== null) {
-            $(this).children(".schedule").val(schedule);
-        }
-    });
-}
-
-startDay()
-
-
-
-// event listener that triggers save buttons to save stuff into local storage
-
-
-
-// function that tells us what timeblock we are in using color, what timeblocks we have already been in by muting color, and what time blocks we have left by using a different color
-
-function futurePastPresent(){
-    
-}
-
-
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// -----------------------------------------------------------------------------------------------//
+//**Display current day on page from Moment.js
+$(“#currentDay”).text(moment().format(“dddd, MMMM Do”));
+// WHEN I scroll down
+// THEN I am presented with time blocks for standard business hours
+// -----------------------------------------------------------------------------------------------//
+//**HTML with standard time blocks==attach an id”9"*/
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+// -----------------------------------------------------------------------------------------------//
+//**Get current number of hours from Moment and set it to a variable currentHour: var currentHour = moment().hours();
+//**Loop through all of the buttons
+//**Declare a variable blockHour and set it to the value of each button id using: parseInt($(this).attr(“id”))
+//**Using if statements, compare currentHour to blockHOur and tag classes “past, presnt, or future” on each button using $(this).addClass(“present”); $(this).removeClass(“past”); $(this).removeClass(“future”) depending on whether currentHour is equal to, greater than, or less than each blockHour
+// WHEN I click into a time block
+// THEN I can enter an event
+// -----------------------------------------------------------------------------------------------//
+//HTML
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+// -----------------------------------------------------------------------------------------------/
+//**Make an event listener for a button click, and when clicked save key: value to local storage */
+//  localStorage.setItem(time, value);
+$(“.saveBtn”).on(“click”, function() {
+//**Declare a Time variable and store time block (button element id--set by hours of the calendar):
+var time = $(this).parent().attr(“id”);
+//**Declare a Value variable
+var value = $(this).siblings(“.description”).val()
+localStorage.setItem(time, value);
+})
+// WHEN I refresh the page
+// THEN the saved events persist
+// -----------------------------------------------------------------------------------------------//
+//**For each text-area class, get data from local storage and populate as text string */
+//  $(“#hour-9 .description”).val(localStorage.getItem(“9”));
